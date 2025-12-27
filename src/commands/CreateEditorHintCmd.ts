47feb3aff5d3bcfccc818557624fa015ad0672f7
fengxu <1964075703@qq.com>
2025-12-27 13:44:59 +0800

    feat(提示文件自动覆盖):在每次插件启动时自动覆盖根目录提示文件

--- src/commands/CreateEditorHintCmd.ts
+++ src/commands/CreateEditorHintCmd.ts
@@ -3,6 +3,13 @@ import * as path from 'path';
 import { promises as fs } from 'fs';
 import * as CommonUtils from '../utils/CommonUtils';
 import { GetHacknetEditorHintFileUri } from "../code-hint/CodeHint";
+
+export enum HintFileExistRule {
+    Overwrite = 0,
+    Abandon = 1,
+    Ask = 2
+}
+
 async function readHacknetDefaultEditorHintFile(context: vscode.ExtensionContext) {
     const filePath = path.join(context.extensionPath, 'templates', 'Hacknet-EditorHint.xml');
     const text = await fs.readFile(filePath, 'utf-8');
@@ -24,7 +31,26 @@ function GetIncludeNodeFromXmlContent(content:string):string {
     return includes.join('\n\t');
 }
 
-export default async function createHacknetEditorHintFileInWorkspaceRoot(context: vscode.ExtensionContext) {
+export async function CheckHacknetEditorHintFileExist() {
+    const workspaceRoot = CommonUtils.GetWorkspaceRootUri();
+    if (workspaceRoot === undefined) {
+        return false;
+    }
+    const filePath = GetHacknetEditorHintFileUri();
+
+    try {
+        await vscode.workspace.fs.stat(filePath);
+        return true;
+    } catch (error) {
+        if (error instanceof vscode.FileSystemError && error.code !== vscode.FileSystemError.FileNotFound().code) {
+            throw error;
+        }
+    }
+
+    return false;
+}
+
+export async function CreateHacknetEditorHintFileInWorkspaceRoot(context: vscode.ExtensionContext, existRule: HintFileExistRule = HintFileExistRule.Ask, notifyUser: boolean = true) {
     const workspaceRoot = CommonUtils.GetWorkspaceRootUri();
     if (workspaceRoot === undefined) {
         return;
@@ -36,7 +62,7 @@ export default async function createHacknetEditorHintFileInWorkspaceRoot(context
     try {
         await vscode.workspace.fs.stat(filePath);
         fileExists = true;
-        oldFileContent = Buffer.from(await vscode.workspace.fs.readFile(filePath)).toString('utf-8'); ;
+        oldFileContent = Buffer.from(await vscode.workspace.fs.readFile(filePath)).toString('utf-8');
     } catch (error) {
         if (error instanceof vscode.FileSystemError && error.code !== vscode.FileSystemError.FileNotFound().code) {
             throw error;
@@ -45,14 +71,19 @@ export default async function createHacknetEditorHintFileInWorkspaceRoot(context
 
     if (fileExists)
     {
-        const selection = await vscode.window.showWarningMessage(
-            `文件:Hacknet-EditorHint.xml 已存在，是否覆盖？`,
-            { modal: true },
-            '覆盖',
-            '取消'
-        );
+        if (existRule === HintFileExistRule.Ask) {
+            const selection = await vscode.window.showWarningMessage(
+                `文件:Hacknet-EditorHint.xml 已存在，是否覆盖？`,
+                { modal: true },
+                '覆盖',
+                '取消'
+            );
 
-        if (selection !== '覆盖') {
+            if (selection !== '覆盖') {
+                return;
+            }
+
+        } else if (existRule === HintFileExistRule.Abandon) {
             return;
         }
     }
@@ -69,7 +100,9 @@ export default async function createHacknetEditorHintFileInWorkspaceRoot(context
             viewColumn: vscode.ViewColumn.One  // 在第一个编辑器组打开
         });
 
-        vscode.window.showInformationMessage(`Hacknet-EditorHint Create Success`);
+        if (notifyUser) {
+            vscode.window.showInformationMessage(`Hacknet-EditorHint Create Success`);
+        }
     } catch (error) {
         vscode.window.showErrorMessage(`Failed to create file: ${error}`);
     }
--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,5 @@
 import * as vscode from 'vscode';
-import createEditorHintFile from './commands/CreateEditorHintCmd';
+import {CheckHacknetEditorHintFileExist, HintFileExistRule, CreateHacknetEditorHintFileInWorkspaceRoot} from './commands/CreateEditorHintCmd';
 import {RegisterHacknetXmlCodeHint} from './code-hint/CodeHint';
 import * as GlobalHacknetXmlNodeHolder from './worker/GlobalHacknetXmlNodeHolder';
 import {RegisterHacknetColorProvider} from './decorator/XmlTextColorProvider';
@@ -10,16 +10,22 @@ import { RegisterCreateHacknetResourceFileCommands } from "./commands/CreateHack
 import { RegiserHacknetThemeView } from "./view/ThemeViewer";
 import { StartDiagnostic } from "./diagnostic/HacknetFileDiagnostic";
 
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
 
 	CommonUtils.SetExtensionContext(context);
+
+	// 每次激活覆盖提示文件如果存在的话
+	if (await CheckHacknetEditorHintFileExist()) {
+		console.log("存在Hacknet-EditorHint文件,将被覆盖");
+		await CreateHacknetEditorHintFileInWorkspaceRoot(context, HintFileExistRule.Overwrite, false);
+	}
 
 	// 代码提示
 	RegisterHacknetXmlCodeHint(context);
 
 	// 注册创建编辑器提示文件命令
 	context.subscriptions.push(vscode.commands.registerCommand('hacknetextensionhelper.createEditorHintFile', () => {
-		createEditorHintFile(context);
+		CreateHacknetEditorHintFileInWorkspaceRoot(context, HintFileExistRule.Ask, true);
 	}));
 
 	// 注册代码提示命令
